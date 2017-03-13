2178122c2dd47fad1b57955c8592e9747b6780db
Chris Eidsmoe <chris.eidsmoe@gmail.com>
2017-03-13 03:13:42 -0500

    physics less jank, but still very fucking jank

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,6 +23,7 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce (movement * speed);
+        //rb.AddForce (movement * speed);
+        transform.Translate (movement * speed * Time.deltaTime);
     }
 }
